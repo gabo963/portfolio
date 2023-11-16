6b47734639819553da9fb43e18b5c6c111a74956
Gabriel <git@github.com>
2023-11-16 11:45:07 -0500

    cambios a main

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hola Mundo</h1>
+      <h3>Cambios que generen conflictos</h3>
+      <h2>Estamos jugando con Andres</h2>
     </div>
   );
 }
